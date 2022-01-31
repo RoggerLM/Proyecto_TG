417bf0bca685c669af5c2ed35fc07e0066ed982b
darkjsc <62622144+darkjsc@users.noreply.github.com>
2022-01-30 19:19:48 -0500

    index actualizado

---
+++ vistas/index.php
@@ -0,0 +1,24 @@
+<?php
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<tittle> Login | Bienestar universitario </tittle>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head> 
+	<body> 
+		<section id="container">
+			<form action="" method="post">
+				<h3>Iniciar Sesión </h3>
+				<img src="assets/img/Login icono.png" alt="Login">
+                <input type="text" name="Usuario" placeholder="Usuario" >
+                <input type="password" name="clave" placeholder="Contraseña">
+                <p class="alert"></p>
+                <input type="submit" value="Ingresar">
+            </form>
+        </section>
+    </body>
+</html>
