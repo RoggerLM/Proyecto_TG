89d0260c200539b494906f2ca7e6fdec69809992
darkjsc <62622144+darkjsc@users.noreply.github.com>
2022-01-30 21:12:00 -0500

    ahora si listo xd el login

--- vistas/index.php
+++ vistas/index.php
@@ -4,6 +4,7 @@
 
 <!DOCTYPE HTML>
 <html>
+<img align="center" src="assets/img/cabecera.png" width="1350" height="300" />
 <head>
 		<title>Inicio de Sesión</title>
 		<link rel="stylesheet" href="css/style_login.css">
@@ -11,21 +12,18 @@
 <body>
 <form action="" method="POST">
   	<div class="container">
-      	<h1>PHP Inicio de Sesión</h1>
-
+      	<h1 align="center">Inicio de Sesión</h1>
+			<img src="assets/img/Login icono.png" width="20" height="20"/>
     		<label for="username"><b>Usuario <span style="color:red">*</span></b></label>
     		<input type="text" placeholder="Username" name="DNI" required>
-
+			<img src="assets/img/contra icon.png" width="20" height="20"/>
     		<label for="password"><b>Contraseña <span style="color:red">*</span></b></label>
     		<input type="password" placeholder="Password" name="Contraseña" required>
 
     		<button type="submit">Login</button>
   	</div>
 
-  	<!--<div class="container" style="background-color:#f1f1f1">
-    		<button type="button" onclick="window.location.href='index.php'" class="cancelbtn">Cancel</button>
-    		<span class="psw"><a href="#">Forgot password?</a></span>
-  	</div>-->
+
 
 </form>
 </body>
