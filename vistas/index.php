a5dc0cdaf3806a7fe38a8a4c83665f2e300a7d1f
darkjsc <62622144+darkjsc@users.noreply.github.com>
2022-01-30 21:57:29 -0500

    validar solo numeros

--- vistas/index.php
+++ vistas/index.php
@@ -1,6 +1,9 @@
 <?php
 
+
+
 ?>
+
 
 <!DOCTYPE HTML>
 <html>
@@ -15,7 +18,7 @@
       	<h1 align="center">Inicio de Sesión</h1>
 			<img src="assets/img/Login icono.png" width="20" height="20"/>
     		<label for="username"><b>Usuario <span style="color:red">*</span></b></label>
-    		<input type="text" placeholder="Username" name="DNI" required>
+    		<input type="text" placeholder="Username" name="DNI" onKeypress="if (event.keyCode < 45 || event.keyCode > 57) event.returnValue = false;" required>
 			<img src="assets/img/contra icon.png" width="20" height="20"/>
     		<label for="password"><b>Contraseña <span style="color:red">*</span></b></label>
     		<input type="password" placeholder="Password" name="Contraseña" required>
